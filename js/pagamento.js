5db60d62d796a22746cdc4557e9b3dbfb9e61bc6
unknown <levysousa6211@digitalcollege.internal>
2025-02-06 21:49:09 -0300

    aula 15

--- js/pagamento.js
+++ js/pagamento.js
@@ -1,12 +1,16 @@
 const INPUT_NUMERO = document.getElementById('card_numero');
 const INPUT_CVV = document.getElementById('card_cvv');
-const INPUT_TITULAR = document.getElementById('card_titular');
+const CARD_TITULAR = document.getElementById('card_titular');
 const SELECT_MES = document.getElementById('card_mes');
 const SELECT_ANO = document.getElementById('card_ano');
 let card_numero = document.getElementById('resultado_numero');
 let resustado_mes = document.getElementById('resultado_mes')
 let resultado_ano = document.getElementById('resultado_ano');
-
+let resultado_nome = document.getElementById('resultado_nome');
+const INPUT_TITULAR = document.getElementById("input_titular");
+let img_master =  document.getElementById('bandeira_mastercard');
+let img_visa = document.getElementById('bandeira_visa');
+const CARTAO_VERSO = document.getElementById('resultado_cvv');
 let hoje = new Date();
 let anoFim = hoje.getFullYear() + 10;
 for(let ano = hoje.getFullYear(); ano <= anoFim; ano++) {
@@ -34,8 +38,13 @@ meses.map((mes) => {
 })
 
 function preencherNumero() {
-    card_numero.innerHTML = INPUT_NUMERO.value;
+    let digitos = INPUT_NUMERO.value;
+    card_numero.innerHTML = digitos;
+    alterarBandeira()
+}
 
+function  removerUltimoDigito(numero) {
+    return numero.substr(0, numero.length - 1)
 }
 
 function preencherMes(evento) {
@@ -48,5 +57,46 @@ function preencherAno(evento) {
     resultado_ano.innerHTML = evento.value
 }
 
+function preencherNome() {
+    resultado_nome.innerHTML = INPUT_TITULAR.value
+}
 
 
+function alterarBandeira() {
+    if(INPUT_NUMERO.value.length > 1) {
+        return;
+    }
+    // Posso verificar se o tamanho de digitos do input é maior que um, ou seja, ele nao executara, maias de uma vez, o document;
+    img_master.style.display = 'none';
+    img_visa.style.display = 'none';
+    if(INPUT_NUMERO.value.startsWith('4')) {
+        img_master.style.display = 'block';
+        img_visa.style.display = 'none';
+    } 
+    if(INPUT_NUMERO.value.startsWith('5')) {
+        img_visa.style.display = 'block';
+        img_master.style.display = 'none';
+    }
+
+} 
+
+function preencherCvv() {
+    CARTAO_VERSO.innerHTML = INPUT_CVV.value;
+}
+
+
+function mostarFrente() {
+    document.getElementById('cartao_frente').classList.add('animate__slideInRight') ;
+
+    document.getElementById('cartao_frente').style.display = 'block';
+    document.getElementById('cartao_verso').style.display = 'none';
+}
+
+function mostrarVerso() {
+    document.getElementById('cartao_verso').classList.add('animate__slideInRight') ;
+
+    document.getElementById('cartao_verso').style.display = 'block ';
+    document.getElementById('cartao_frente').style.display = 'none';
+
+
+}
